a6c898e125525972606ca7c17303feff9e21717e
lalifeier <lalifeier@gmail.com>
2022-09-18 23:52:49 +0800

    init

--- mall-common/src/main/java/com/github/lalifeier/mall/common/exception/GlobalExceptionHandler.java
+++ mall-common/src/main/java/com/github/lalifeier/mall/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,9 @@
 package com.github.lalifeier.mall.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 }
