34d51142ca12563c87b964e75e90cd8abe19fa67
lalifeier <lalifeier@gmail.com>
2022-09-19 23:09:03 +0800

    init

--- deploy/sentinel/entrypoint.sh
+++ deploy/sentinel/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-java -Dserver.port=8080 -Dcsp.sentinel.dashboard.server=localhost:8080 -Dproject.name=sentinel-dashboard -jar $SENTINEL_HOME/sentinel-dashboard.jar
+java -Dserver.port=8858 -Dcsp.sentinel.dashboard.server=localhost:8858 -Dproject.name=sentinel-dashboard -jar $SENTINEL_HOME/sentinel-dashboard.jar
