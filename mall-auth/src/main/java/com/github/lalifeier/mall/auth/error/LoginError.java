8533bfd27f39312de03c02265076b8e154973fd3
lalifeier <lalifeier@gmail.com>
2022-10-10 00:30:47 +0800

    init

--- mall-auth/src/main/java/com/github/lalifeier/mall/auth/error/LoginError.java
+++ mall-auth/src/main/java/com/github/lalifeier/mall/auth/error/LoginError.java
@@ -1,27 +1,28 @@
 package com.github.lalifeier.mall.auth.error;
 
 
-import com.github.lalifeier.api.ErrorCode;
+import com.github.lalifeier.api.IError;
 import com.github.lalifeier.manager.ErrorManager;
 import lombok.Getter;
 
 @Getter
-public enum LoginErrorCodes implements ErrorCode {
+public enum LoginError implements IError {
     /**
      * 用户不存在
      */
     USER_NOT_EXIST(0, "用户名不存在"),
+
     /**
      * 密码错误
      */
     PASSWORD_ERROR(1, "密码错误");
 
     private final int nodeNum;
-    private final String msg;
+    private final String message;
 
-    LoginErrorCodes(int nodeNum, String msg) {
+    LoginError(int nodeNum, String message) {
         this.nodeNum = nodeNum;
-        this.msg = msg;
+        this.message = message;
         ErrorManager.register(UserProjectCodes.USER, this);
     }
 }
