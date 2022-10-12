0e294b05ebd3a54a3b8a0a9b0e927fa4cb7eacc0
lalifeier <lalifeier@gmail.com>
2022-10-12 18:26:55 +0800

    init

--- mall-account/src/main/java/com/github/lalifeier/mall/account/controller/HelloController.java
+++ mall-account/src/main/java/com/github/lalifeier/mall/account/controller/HelloController.java
@@ -1,13 +1,27 @@
 package com.github.lalifeier.mall.account.controller;
 
+
+import com.github.lalifeier.mall.account.vo.ValidVO;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
+
+//        Assert.notNull(null, "");
+
+        return "Hello World !";
+    }
+
+    @PostMapping("/valid")
+    public String valid(@Validated @RequestBody ValidVO validVO) {
         return "Hello World !";
     }
 
--- mall-common-starter/mall-common-spring-boot-starter/src/main/java/com/github/lalifeier/advice/GlobalExceptionAdvice.java
+++ mall-common-starter/mall-common-spring-boot-starter/src/main/java/com/github/lalifeier/advice/GlobalExceptionAdvice.java
@@ -1,8 +1,8 @@
 package com.github.lalifeier.advice;
 
 
-import com.github.lalifeier.manager.ErrorInfo;
 import com.github.lalifeier.api.Response;
+import com.github.lalifeier.manager.ErrorInfo;
 import com.github.lalifeier.system.HttpCodes;
 import com.github.lalifeier.system.SystemError;
 import com.google.common.base.Joiner;
@@ -12,16 +12,19 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -45,27 +48,44 @@ public class GlobalExceptionAdvice {
             } else {
                 apiResult = Response.failure(errorInfo.getCode(), errorInfo.getMsg());
             }
-            return new ResponseEntity<>(apiResult, HttpStatus.OK);
+            return new ResponseEntity<>(apiResult, HttpStatus.INTERNAL_SERVER_ERROR);
         }
         log.error("error, request: {}", parseParam(request), e);
         Response<String> errorResult = Response.failure(SystemError.SYSTEM_ERROR.getCode(), pair.getLeft().getClass().getSimpleName() + ": " + pair.getRight());
-        return new ResponseEntity<>(errorResult, HttpStatus.OK);
+        return new ResponseEntity<>(errorResult, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    /**
-     * 请求参数异常
-     */
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<Response<String>> badRequestException(HttpServletRequest request, MethodArgumentNotValidException e) {
-        BindingResult bindingResult = e.getBindingResult();
-        StringBuilder builder = new StringBuilder();
-        for (FieldError fieldError : bindingResult.getFieldErrors()) {
-            builder.append(fieldError.getField() + fieldError.getDefaultMessage()).append(", ");
+    @ExceptionHandler(value = {BindException.class, ConstraintViolationException.class, MethodArgumentNotValidException.class})
+    public ResponseEntity<Response<String>> handleValidatedException(HttpServletRequest request, Exception e) {
+//        BindingResult bindingResult = e.getBindingResult();
+//        StringBuilder builder = new StringBuilder();
+//        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+//            builder.append(fieldError.getField() + fieldError.getDefaultMessage()).append(", ");
+//        }
+
+        String message = "";
+        if (e instanceof MethodArgumentNotValidException) {
+            MethodArgumentNotValidException ex = (MethodArgumentNotValidException) e;
+            message =
+                    ex.getBindingResult().getAllErrors().stream()
+                            .map(ObjectError::getDefaultMessage)
+                            .collect(Collectors.joining(", "));
+        } else if (e instanceof ConstraintViolationException) {
+            ConstraintViolationException ex = (ConstraintViolationException) e;
+            message =
+                    ex.getConstraintViolations().stream()
+                            .map(ConstraintViolation::getMessage)
+                            .collect(Collectors.joining(", "));
+        } else if (e instanceof BindException) {
+            BindException ex = (BindException) e;
+            message = ex.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
         }
-        log.error("BadRequestException, request: {}", parseParam(request), e);
-        return new ResponseEntity<>(Response.failure(HttpCodes.BAD_REQUEST.getStatus(), builder.toString()), HttpStatus.OK);
-    }
 
+        log.error("BadRequestException, request: {}", parseParam(request), e);
+        return new ResponseEntity<>(Response.failure(HttpCodes.BAD_REQUEST.getStatus(), message), HttpStatus.BAD_REQUEST);
+    }
 
     public String parseParam(HttpServletRequest request) {
         Map<String, String[]> parameterMap = request.getParameterMap();
