167c80a6507bde4785682e65ddedbe54b2ab0a56
lalifeier <lalifeier@gmail.com>
2022-10-14 18:21:48 +0800

    init

---
+++ mall-component/mall-common-spring-boot-starter/src/main/java/com/github/lalifeier/handler/RestResponseHandler.java
@@ -0,0 +1,60 @@
+package com.github.lalifeier.handler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.lalifeier.annotation.IgnoreResponseAdvice;
+import com.github.lalifeier.api.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.util.Objects;
+
+
+@Slf4j
+@RestControllerAdvice(basePackages = "com.github.lalifeier")
+public class RestResponseHandler implements ResponseBodyAdvice<Object> {
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        if (returnType.getDeclaringClass().isAnnotationPresent(IgnoreResponseAdvice.class)) {
+            return false;
+        }
+        if (Objects.requireNonNull(returnType.getMethod()).isAnnotationPresent(IgnoreResponseAdvice.class)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if (body == null) {
+            return Response.success();
+        }
+
+        if (body instanceof String) {
+            try {
+                response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+                return objectMapper.writeValueAsString(Response.success(body));
+            } catch (JsonProcessingException e) {
+                log.error(e.getMessage(), e);
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }
+
+        if (body instanceof Response) {
+            return body;
+        }
+
+        return Response.success(body);
+    }
+}
