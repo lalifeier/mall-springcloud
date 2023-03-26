13ee776273a67a907234880105fa698ee5dc5f6c
lalifeier <lalifeier@gmail.com>
2023-03-26 23:35:04 +0800

    init

---
+++ mall-kotlin-demo/src/main/kotlin/com/github/lalifeier/mall/kotlin/demo/MallKotlinDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.github.lalifeier.mall.kotlin.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MallKotlinDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<MallKotlinDemoApplication>(*args)
+}
---
+++ mall-kotlin-demo/src/test/kotlin/com/github/lalifeier/mall/kotlin/demo/MallKotlinDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.lalifeier.mall.kotlin.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MallKotlinDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
