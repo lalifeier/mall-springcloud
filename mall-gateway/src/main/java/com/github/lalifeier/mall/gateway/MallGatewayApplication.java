c73a4ca23660a01398476aad3eb8e2d14a9a03f8
lalifeier <lalifeier@gmail.com>
2023-03-26 19:32:28 +0800

    init

--- mall-gateway/src/main/java/com/github/lalifeier/mall/gateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/github/lalifeier/mall/gateway/MallGatewayApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MallGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MallGatewayApplication.class, args);
 	}
