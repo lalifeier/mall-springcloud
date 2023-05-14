dff796aa7bbdd3aa989bef1a45d1c5f64f3f1dcd
lalifeier <lalifeier@gmail.com>
2023-05-14 16:49:24 +0800

    init

---
+++ mall-gateway/src/main/java/com/github/lalifeier/mall/gateway/infrastructure/filter/GlobalUriFilter.java
@@ -0,0 +1,62 @@
+// https://github.com/spring-cloud/spring-cloud-gateway/issues/2065
+
+package com.github.lalifeier.mall.gateway.infrastructure.filter;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
+
+import java.net.URI;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.RouteToRequestUrlFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import lombok.var;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GlobalUriFilter implements GlobalFilter, Ordered {
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    URI incomingUri = exchange.getRequest().getURI();
+    if (isUriEncoded(incomingUri)) {
+      // Get the original Gateway route (contains the service's original host)
+      Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+      if (route == null) {
+        return chain.filter(exchange);
+      }
+
+      // Use the original incomingUri path and query params
+      final var routeUri = route.getUri();
+      URI mergedUri = createUri(incomingUri, routeUri);
+
+      // Save it as the outgoing URI to call the service, and override the "wrongly"
+      // double encoded URI
+      exchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, mergedUri);
+    }
+
+    return chain.filter(exchange);
+  }
+
+  private URI createUri(URI incomingUri, URI routeUri) {
+    final var port = routeUri.getPort() != -1 ? ":" + routeUri.getPort() : "";
+    final var rawPath = incomingUri.getRawPath() != null ? incomingUri.getRawPath() : "";
+    final var query = incomingUri.getRawQuery() != null ? "?" + incomingUri.getRawQuery() : "";
+    return URI.create(routeUri.getScheme() + "://" + routeUri.getHost() + port + rawPath + query);
+  }
+
+  private static boolean isUriEncoded(URI uri) {
+    return (uri.getRawQuery() != null && uri.getRawQuery().contains("%"))
+        || (uri.getRawPath() != null && uri.getRawPath().contains("%"));
+  }
+
+  @Override
+  public int getOrder() {
+    return RouteToRequestUrlFilter.ROUTE_TO_URL_FILTER_ORDER + 1;
+  }
+}
