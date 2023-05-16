29b681d9013764bc4c44c3b71483d14b79262807
lalifeier <lalifeier@gmail.com>
2023-05-16 23:51:11 +0800

    init

---
+++ docs/sql/user_center.sql
@@ -0,0 +1,95 @@
+CREATE TABLE `account_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `username` varchar(30) NOT NULL DEFAULT '' COMMENT '用户名',
+  `password` varchar(60) NOT NULL DEFAULT '' COMMENT '密码',
+  `phone` varchar(15) NOT NULL DEFAULT '' COMMENT '手机号',
+  `email` varchar(30) NOT NULL DEFAULT '' COMMENT '邮箱',
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '帐号状态：0 - 无效，1 - 有效',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `created_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建人',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `updated_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改人',
+  `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否删除：0未删除，1已删除',
+  PRIMARY KEY (`id`),
+  KEY `idx_username` (`username`),
+  KEY `idx_phone` (`phone`),
+  KEY `idx_email` (`email`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='帐号表';
+
+CREATE TABLE `account_platform` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `account_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '账号id',
+  `platform_id` varchar(60) NOT NULL DEFAULT '' COMMENT '平台id',
+  `platform_token` varchar(60) NOT NULL DEFAULT '' COMMENT '平台access_token',
+  `type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '平台类型 0:未知,1:facebook,2:google,3:wechat,4:qq,5:weibo,6:twitter',
+  `nickname` varchar(60) NOT NULL DEFAULT '' COMMENT '昵称',
+  `avatar` varchar(255) NOT NULL DEFAULT '' COMMENT '头像',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `created_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建人',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `updated_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改人',
+  `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否删除：0未删除，1已删除',
+  PRIMARY KEY (`id`),
+  KEY `idx_account_id` (`account_id`),
+  KEY `idx_platform_id` (`platform_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='第三方帐号信息';
+
+CREATE TABLE `account_device` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '记录ID',
+  `account_id` INT UNSIGNED NOT NULL COMMENT '帐号ID',
+  `app_id` INT UNSIGNED NOT NULL COMMENT '应用ID',
+  `app_user_id` INT UNSIGNED NOT NULL COMMENT '应用用户ID',
+  `device_type` TINYINT(1) NOT NULL DEFAULT '0' COMMENT '设备类型：0 - 未知，1 - 手机，2 - 平板电脑，3 - PC',
+  `device_fingerprint` VARCHAR(255) NOT NULL COMMENT '设备指纹',
+  `device_info` JSON NOT NULL DEFAULT '{}' COMMENT '设备信息',
+  `login_ip` VARCHAR(45) NOT NULL COMMENT '登录IP地址',
+  `is_login` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否登录：0未登录，1已登录',
+  `login_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '登录时间',
+  `logout_at` DATETIME DEFAULT NULL COMMENT '登出时间',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `created_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建人',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `updated_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改人',
+  `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否删除：0未删除，1已删除',
+  PRIMARY KEY (id),
+  KEY `idx_account_id` (`account_id`),
+  KEY `idx_device_fingerprint` (`device_fingerprint`),
+  KEY `idx_login_ip` (`login_ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='帐号设备信息表';
+
+CREATE TABLE `staff` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `account_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '账号id',
+    `name` varchar(30) NOT NULL DEFAULT '' COMMENT '员工姓名',
+    `phone` varchar(15) NOT NULL DEFAULT '' COMMENT '员工手机号',
+    `email` varchar(30) NOT NULL DEFAULT '' COMMENT '员工邮箱',
+    `nickname` varchar(30) NOT NULL DEFAULT '' COMMENT '员工昵称',
+    `avatar` varchar(255) NOT NULL DEFAULT '' COMMENT '员工头像'
+    `gender` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '员工性别 1:男性 2:女性',
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `created_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '更新人',
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `updated_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '更新人',
+    PRIMARY KEY (`id`),
+    KEY `idx_account_id` (`account_id`),
+    KEY `idx_phone` (`phone`),
+    KEY `idx_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='员工表';
+
+
+CREATE TABLE `xxx_app_user` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `account_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '账号id',
+  `app_id` INT UNSIGNED NOT NULL COMMENT '应用ID',
+  `nickname` varchar(30) NOT NULL DEFAULT '' COMMENT '昵称',
+  `avatar` varchar(255) NOT NULL DEFAULT '' COMMENT '头像',
+  `gender` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '性别 1:男性 2:女性',
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `created_by` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '创建人',
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `updated_by` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '修改人',
+  `is_deleted` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否删除：0未删除，1已删除',
+  PRIMARY KEY (id),
+  KEY `idx_account_id` (`account_id`),
+  KEY `idx_app_id` (`app_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='应用用户表';
