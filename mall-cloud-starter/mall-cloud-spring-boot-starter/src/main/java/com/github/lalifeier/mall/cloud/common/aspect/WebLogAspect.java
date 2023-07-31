cbda9ce1ab52f0eed8455247fecd2c5b95cf0d5c
lalifeier <lalifeier@gmail.com>
2023-07-31 23:43:39 +0800

    init

--- mall-cloud-starter/mall-cloud-spring-boot-starter/src/main/java/com/github/lalifeier/mall/cloud/common/aspect/WebLogAspect.java
+++ mall-cloud-starter/mall-cloud-spring-boot-starter/src/main/java/com/github/lalifeier/mall/cloud/common/aspect/WebLogAspect.java
@@ -3,10 +3,6 @@ package com.github.lalifeier.mall.cloud.common.aspect;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.lalifeier.mall.cloud.common.utils.WebUtil;
 import jakarta.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Map;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -14,80 +10,92 @@ import org.aspectj.lang.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Map;
+
 @Slf4j
 @Aspect
 @Component
 public class WebLogAspect {
 
-    @Autowired private ObjectMapper objectMapper;
+  @Autowired
+  private ObjectMapper objectMapper;
 
-    private final ThreadLocal<RequestInfo> THREAD_LOCAL = new ThreadLocal<>();
+  private final ThreadLocal<WebLog> THREAD_LOCAL = new ThreadLocal<>();
 
-    @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
-    public void webLog() {}
+  @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
+  public void webLog() {
+  }
 
-    @Before("webLog()")
-    public void doBefore(JoinPoint joinPoint) throws IOException {
-        HttpServletRequest request = WebUtil.getRequest();
-        RequestInfo requestInfo = new RequestInfo();
-        requestInfo.setRequestTime(LocalDateTime.now());
-        requestInfo.setProtocol(request.getProtocol());
-        requestInfo.setSchema(request.getScheme());
-        requestInfo.setIp(request.getRemoteAddr());
-        requestInfo.setSessionId(request.getSession().getId());
-        requestInfo.setUrl(request.getRequestURL().toString());
-        requestInfo.setHttpMethod(request.getMethod());
-        requestInfo.setClassMethod(
-                String.format(
-                        "%s.%s",
-                        joinPoint.getSignature().getDeclaringTypeName(),
-                        joinPoint.getSignature().getName()));
-        requestInfo.setHeader(WebUtil.getRequestHeaders(request));
-        requestInfo.setQuery(WebUtil.getRequestQuery(request));
-        requestInfo.setPayload(WebUtil.getRequestPayload(request));
-        THREAD_LOCAL.set(requestInfo);
-    }
+  @Before("webLog()")
+  public void doBefore(JoinPoint joinPoint) {
+    HttpServletRequest request = WebUtil.getRequest();
+    WebLog webLog = new WebLog();
+    webLog.setRequestTime(LocalDateTime.now());
+    webLog.setProtocol(request.getProtocol());
+    webLog.setSchema(request.getScheme());
+    webLog.setIp(request.getRemoteAddr());
+    webLog.setUserAgent(request.getHeader("User-Agent"));
+    webLog.setSessionId(request.getSession().getId());
+    webLog.setUrl(request.getRequestURL().toString());
+    webLog.setHttpMethod(request.getMethod());
+    webLog.setClassMethod(
+      String.format(
+        "%s.%s",
+        joinPoint.getSignature().getDeclaringTypeName(),
+        joinPoint.getSignature().getName()));
+    webLog.setHeader(WebUtil.getRequestHeaders(request));
+    webLog.setQuery(WebUtil.getRequestQuery(request));
+    webLog.setPayload(WebUtil.getRequestPayload(request));
+    THREAD_LOCAL.set(webLog);
+  }
 
-    @AfterReturning(value = "webLog()", returning = "ret")
-    public void doAfterReturning(Object ret) throws Throwable {
-        RequestInfo requestInfo = THREAD_LOCAL.get();
-        requestInfo.setTimeCost(
-                System.currentTimeMillis()
-                        - requestInfo
-                                .getRequestTime()
-                                .atZone(ZoneId.systemDefault())
-                                .toInstant()
-                                .toEpochMilli());
-        log.info(objectMapper.writeValueAsString(requestInfo));
-    }
+  @AfterReturning(value = "webLog()", returning = "ret")
+  public void doAfterReturning(Object ret) throws Throwable {
+    WebLog webLog = THREAD_LOCAL.get();
+    webLog.setTimeCost(
+      System.currentTimeMillis()
+        - webLog
+        .getRequestTime()
+        .atZone(ZoneId.systemDefault())
+        .toInstant()
+        .toEpochMilli());
+    webLog.setResponseInfo(objectMapper.writeValueAsString(ret));
+    log.info(objectMapper.writeValueAsString(webLog));
+  }
 
-    @AfterThrowing(pointcut = "webLog()", throwing = "e")
-    public void doAfterThrow(JoinPoint joinPoint, Throwable e) throws Throwable {
-        RequestInfo requestInfo = THREAD_LOCAL.get();
-        requestInfo.setTimeCost(
-                System.currentTimeMillis()
-                        - requestInfo
-                                .getRequestTime()
-                                .atZone(ZoneId.systemDefault())
-                                .toInstant()
-                                .toEpochMilli());
-        log.info(objectMapper.writeValueAsString(requestInfo));
-    }
+  @AfterThrowing(pointcut = "webLog()", throwing = "e")
+  public void doAfterThrow(JoinPoint joinPoint, Throwable e) throws Throwable {
+    WebLog webLog = THREAD_LOCAL.get();
+    webLog.setTimeCost(
+      System.currentTimeMillis()
+        - webLog
+        .getRequestTime()
+        .atZone(ZoneId.systemDefault())
+        .toInstant()
+        .toEpochMilli());
+    webLog.setExceptionInfo(e.getMessage());
+    log.info(objectMapper.writeValueAsString(webLog));
+  }
 
-    @Data
-    public class RequestInfo {
-        //    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        private LocalDateTime requestTime;
-        private String classMethod;
-        private String httpMethod;
-        private String protocol;
-        private String schema;
-        private String ip;
-        private String sessionId;
-        private String url;
-        private Map<String, String> header;
-        private Map<String, String> query;
-        private String payload;
-        private Long timeCost;
-    }
+  @Data
+  public class WebLog {
+    //    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime requestTime;
+    private String classMethod;
+    private String httpMethod;
+    private String protocol;
+    private String schema;
+    private String ip;
+    private String userAgent;
+    private String sessionId;
+    private String url;
+    private Map<String, String> header;
+    private Map<String, String> query;
+    private String payload;
+    private Long timeCost;
+    private String responseInfo;
+    private String exceptionInfo;
+  }
 }
