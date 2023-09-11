3473a06311044709a6d4a41efed744e9f1fcbb47
lalifeier <lalifeier@gmail.com>
2023-09-12 00:00:38 +0800

    init

---
+++ mall-cloud-common/mall-cloud-common-core/src/main/java/com/github/lalifeier/mall/cloud/common/utils/reflect/AnnotationUtil.java
@@ -0,0 +1,9 @@
+package com.github.lalifeier.mall.cloud.common.utils.reflect;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AnnotationUtil {
+
+}
