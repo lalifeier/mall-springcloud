698c6a5425d0ff650fc7a35747a3d5b796e13f9c
lalifeier <lalifeier@gmail.com>
2023-09-15 00:05:09 +0800

    init

--- mall-cloud-starter/mall-cloud-spring-boot-starter/src/main/java/com/github/lalifeier/mall/cloud/common/jackson/LongToStingSerializer.java
+++ mall-cloud-starter/mall-cloud-spring-boot-starter/src/main/java/com/github/lalifeier/mall/cloud/common/jackson/LongToStingSerializer.java
@@ -1,22 +1,20 @@
 package com.github.lalifeier.mall.cloud.common.jackson;
 
-import java.io.IOException;
-
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
 
 public class LongToStingSerializer extends JsonSerializer<Long> {
 
-  public static final LongToStingSerializer INSTANCE = new LongToStingSerializer();
+    public static final LongToStingSerializer INSTANCE = new LongToStingSerializer();
 
-  @Override
-  public void serialize(Long value, JsonGenerator gen, SerializerProvider serializers)
-      throws IOException {
-    if (value == null) {
-      gen.writeString("");
-    } else {
-      gen.writeString(String.valueOf(value));
+    @Override
+    public void serialize(Long value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        if (value == null) {
+            gen.writeString("");
+        } else {
+            gen.writeString(String.valueOf(value));
+        }
     }
-  }
 }
