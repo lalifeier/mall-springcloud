e6fca6bc118c62cb9e95bf3c61b598c8805e476c
lalifeier <lalifeier@gmail.com>
2023-09-15 18:26:18 +0800

    init

--- mall-cloud-common/mall-cloud-common-core/src/main/java/com/github/lalifeier/mall/cloud/common/utils/EnumUtil.java
+++ mall-cloud-common/mall-cloud-common-core/src/main/java/com/github/lalifeier/mall/cloud/common/utils/EnumUtil.java
@@ -2,11 +2,10 @@ package com.github.lalifeier.mall.cloud.common.utils;
 
 import com.github.lalifeier.mall.cloud.common.annocation.EnumValue;
 import com.github.lalifeier.mall.cloud.common.enums.BaseEnum;
-import lombok.extern.slf4j.Slf4j;
-
 import java.lang.reflect.Field;
 import java.util.*;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 实用工具类，用于处理枚举类型
